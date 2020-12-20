504eb233e685a36456b69d6399f2749713463319
aadi-mishra <aadimishra94@gmail.com>
2020-12-20 16:11:02 +0100

    " Task 4_2 done as SimpleTurner "

--- src/de/tuhh/diss/lab/sheet4/MainSimulated.java
+++ src/de/tuhh/diss/lab/sheet4/MainSimulated.java
@@ -7,15 +7,17 @@ import lejos.utility.Delay;
 public class MainSimulated {
 
 	public static void main(String[] args) {
-		MazebotSimulation sim = new MazebotSimulation("Mazes/maze_1_3by4.png", 1.5,  1.5);
+		MazebotSimulation sim = new MazebotSimulation("Mazes/TestArea.png", 1.5,  1.5);
 		GuiMazeVisualization gui = new GuiMazeVisualization(1.5, sim.getStateAccessor());
 		sim.scaleSpeed(1);
-		sim.setRobotPosition(0.525, 0.525, 90);
+//		sim.setRobotPosition(0.525, 0.525, 90);
+		sim.setRobotPosition(0.75, 0.75, 90);
 
 		sim.startSimulation();
 		gui.startVisualization();
 
 		// Here goes your Code!
+		SimpleTurnerTest.main(new String[0]);
 		
 		Delay.msDelay(100);
 		sim.stopSimulation();
--- src/de/tuhh/diss/lab/sheet4/Turner.java
+++ src/de/tuhh/diss/lab/sheet4/Turner.java
@@ -5,15 +5,4 @@ public interface Turner {
 	public void setSpeed(int degreesPerSecond);
 	public void turn(int degrees);
 
-}
-
-class Rotate implements Turner{
-	
-	public void turn(int degrees) {
-		
-	}
-
-	public void setSpeed(int degreesPerSecond) {
-		
-	}
-}
+}  
